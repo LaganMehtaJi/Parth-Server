4090890da22688415b852add89fa40482ab91e30
Lagan Mehta <laganmehta.in@gmail.com>
2025-07-21 15:13:53 +0530

    Create Admin DashBord

--- parth-server/index.js
+++ parth-server/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import morgan from "morgan";
 import ConnectDb from "./config/db.js";
 import Comapny from "./routes/student/apply.routes.js";
 const app = express();
 dotenv.config();
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use("/api/company",Comapny);
--- parth/src/App.jsx
+++ parth/src/App.jsx
@@ -3,6 +3,7 @@ import {Routes,Route} from "react-router-dom";
 import Hero from "../src/Hero/index.jsx";
 import Home from "../src/Home/index.jsx";
 import Form from "../src/Form/index.jsx";
+import Admin from "../src/adminDashboard/admin.jsx";
 function App() {
 
 
@@ -12,6 +13,7 @@ function App() {
     <Route path='/' element={<Hero/>} />
     <Route path='/home' element={<Home/>} />
      <Route path='/form' element={<Form/>} />
+      <Route path='/admin' element={<Admin/>} />
    </Routes>
     </>
   )
