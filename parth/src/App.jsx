ebafc921b0ae06d9688591f4804e640151f412e4
Lagan Mehta <laganmehta.in@gmail.com>
2025-07-21 11:44:02 +0530

    Create Form for Pankaj

--- parth/src/App.jsx
+++ parth/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Routes,Route} from "react-router-dom";
 import Hero from "../src/Hero/index.jsx";
 import Home from "../src/Home/index.jsx";
+import Form from "../src/Form/index.jsx";
 function App() {
 
 
@@ -10,7 +11,7 @@ function App() {
    <Routes>
     <Route path='/' element={<Hero/>} />
     <Route path='/home' element={<Home/>} />
-    
+     <Route path='/form' element={<Form/>} />
    </Routes>
     </>
   )
