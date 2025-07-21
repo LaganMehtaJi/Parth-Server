674d8408b8be81d1b8177858cd0af161d349572b
Lagan Mehta <laganmehta.in@gmail.com>
2025-07-21 13:00:31 +0530

    Create Company Schema and Route

--- parth-server/index.js
+++ parth-server/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 import ConnectDb from "./config/db.js";
-import Resume from "./routes/resume/resume.routes.js";
+import Comapny from "./routes/student/apply.routes.js";
 const app = express();
 dotenv.config();
-app.set("view engine", "ejs");
+app.use(morgan('dev'));
 app.use(express.json());
-app.use("/resume",Resume);
+app.use("/api/company",Comapny);
+
 
 ConnectDb();
 app.listen(process.env.PORT,(err)=>{
